66594312f154c38630b9993493415c8e65be7fb9
Ryotruuser <ryonantruuser@gmail.com>
2025-02-14 19:28:31 -0300

    coletada todas as imagens do projeto, adicionada interacao com coracao para favoritar o imovel, optei por usar js por ser mais pratico nessa parte

---
+++ js/frufru.js
@@ -0,0 +1,13 @@
+like = () => {
+    emptyHeart = document.getElementById("emptyHeart");
+    fullHeart = document.getElementById("fullHeart");
+    emptyHeart.style.display = "none";    
+    fullHeart.style.display = "inline";
+}
+
+dislike = () => {
+    emptyHeart = document.getElementById("emptyHeart");
+    fullHeart = document.getElementById("fullHeart");
+    emptyHeart.style.display = "inline";    
+    fullHeart.style.display = "none";
+} 
