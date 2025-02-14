f6a24f77bf7cf0060599846f8e0c3f4a35b126a6
Ryotruuser <ryonantruuser@gmail.com>
2025-02-14 19:54:32 -0300

    criado e configurado link externo para funcionar nos cards dos imoveis

--- js/frufru.js
+++ js/frufru.js
@@ -1,13 +1,12 @@
+const emptyHeart = document.getElementById("emptyHeart");
+const fullHeart = document.getElementById("fullHeart");
+
 like = () => {
-    emptyHeart = document.getElementById("emptyHeart");
-    fullHeart = document.getElementById("fullHeart");
     emptyHeart.style.display = "none";    
     fullHeart.style.display = "inline";
 }
 
 dislike = () => {
-    emptyHeart = document.getElementById("emptyHeart");
-    fullHeart = document.getElementById("fullHeart");
     emptyHeart.style.display = "inline";    
     fullHeart.style.display = "none";
 } 
